22bc814b6084ae98fd7d4dd5ee0d8c1b7803e13c
Carsten Buchberger <c.buchberger@witcom.de>
2018-11-08 13:54:05 +0000

    Added JWT-Interceptor

---
+++ camunda-external-worker-nodejs/lib/KeycloakJWTInterceptor.js
@@ -0,0 +1,57 @@
+const Keycloak = require('keycloak-connect');
+
+
+class KeycloakJWTInterceptor {
+
+   /**
+   * @throws Error
+   */
+  constructor(kcConfig) {
+    this.grant = null;
+    this.keycloak = new Keycloak({},kcConfig);
+      
+    if (!kcConfig) {
+      throw new Error("Missing Keycloak-Config");
+    }
+    
+    /**
+     * Initialise Keycloak-Session
+     */
+    this.keycloak.grantManager.obtainFromClientCredentials()
+    .then((newGrant) => {
+        this.grant = newGrant;
+        this.header = this.getHeader();
+    })
+    .catch((error) => {
+        throw new Error("Error initialising KeycloakJWTInterceptor ",error);
+    });
+    
+    /**
+     * Bind member methods
+     */
+    this.getHeader = this.getHeader.bind(this);
+    this.interceptor = this.interceptor.bind(this);
+
+    return this.interceptor;
+    }
+
+  getHeader() {
+      if (!this.grant){
+        console.log("No token yet....");
+        return {};
+        }
+      this.keycloak.grantManager.ensureFreshness(this.grant).then((refreshdetails) => {
+        this.grant=refreshdetails;
+      });
+    const token = this.grant.access_token.token;
+    return { Authorization: `Bearer ${token}` };
+  }
+
+  interceptor(config) {
+    this.header =this.getHeader();
+    return { ...config, headers: { ...config.headers, ...this.header } };
+  }    
+    
+    
+}
+module.exports = KeycloakJWTInterceptor;
--- camunda-external-worker-nodejs/worker.js
+++ camunda-external-worker-nodejs/worker.js
@@ -5,14 +5,18 @@ const {
   File
 } = require("camunda-external-task-client-js");
 
+const KeycloakJWTInterceptor = require("./lib/KeycloakJWTInterceptor")
+
+const kcConfig = require('./config/keycloak.json');
+
+const interceptor = new KeycloakJWTInterceptor(kcConfig);
 // configuration for the Client:
 //  - 'baseUrl': url to the Workflow Engine
 //  - 'logger': utility to automatically log important events
-const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger,workerId:'order-worker-01' };
+const config = { baseUrl: "http://apis.witcom-dev.services/camunda", use: logger,workerId:'order-worker-01',interceptors: interceptor};
 
 // create a Client instance with custom configuration
 const client = new Client(config);
-
 // susbscribe to the topic: 'creditScoreChecker'
 client.subscribe("orderWorker", async function({ task, taskService }) {
   // Put your business logic
